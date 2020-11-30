6d7c53c99d0f11f0a8017653c22a85c216510107
malfonsgas <68823667+malfonsgas@users.noreply.github.com>
2020-11-30 16:32:33 +0100

    Update 1-AGUA-comprobar-2.js

--- js/1-AGUA-comprobar-2.js
+++ js/1-AGUA-comprobar-2.js
@@ -86,7 +86,7 @@ if (Respuesta4=="PD"){
 	}
 
 
-	if ((Respuesta5=="240.5")||(Respuesta5=="240,5")){
+	if ((Respuesta5=="8,658")||(Respuesta5=="8.658")){
 	 	document.getElementById("Correcto5").style='display:block';
 	 	document.getElementById("Incorrecto5").style='display:none';
 	} else{
