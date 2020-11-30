3d5d9e3f3177684d20438ebae3011403ab22570b
malfonsgas <68823667+malfonsgas@users.noreply.github.com>
2020-11-30 16:55:44 +0100

    Update 1-AGUA-comprobar-3.js

--- js/1-AGUA-comprobar-3.js
+++ js/1-AGUA-comprobar-3.js
@@ -86,7 +86,7 @@ if (Respuesta4=="PD"){
 	}
 
 
-	if ((Respuesta5=="9.99") || (Respuesta5=="9,99")){
+	if (Respuesta5=="10"){
 	 	document.getElementById("Correcto5").style='display:block';
 	 	document.getElementById("Incorrecto5").style='display:none';
 	} else{
